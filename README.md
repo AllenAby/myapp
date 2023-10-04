ff4c833fd32524a16529198a98fb737bf9503bef
Allen Aby <allen.aby@gmail.com>
2023-10-04 17:23:21 -0400

    clean up and add readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ This example is described [here](https://rlbox.dev/chapters/wasm-sandbox.html).
 
 Have updated main.cpp, Makefile to reflect stable build.
 
-Assumes rlbox_wasm2c_sandbox has been built and is located at same level as mparent directory -> `RLBOX_ROOT`
+Assumes rlbox and rlbox_wasm2c_sandbox have been built as specified in parent README
--- main.cpp
+++ main.cpp
@@ -22,8 +22,8 @@ using namespace rlbox;
 // Define base type for mylib using the noop sandbox
 RLBOX_DEFINE_BASE_TYPES_FOR(mylib, wasm2c);
 
-// // Declare callback function we're going to call from sandboxed code.
-// void hello_cb(rlbox_sandbox_mylib& _, tainted_mylib<const char*> str);
+// Declare callback function we're going to call from sandboxed code.
+void hello_cb(rlbox_sandbox_mylib& _, tainted_mylib<const char*> str);
 
 int main(int argc, char const *argv[]) {
 // ANCHOR_END: imports
@@ -56,34 +56,30 @@ int main(int argc, char const *argv[]) {
 //   printf("OK? = %d\n", ok);
 // // ANCHOR_END: add
 
-
 // hello_from_rust();
-
 // int sum = sum_from_rust(3, 4);
-
 // printf("sum is %d", sum);
-
 // sandbox.invoke_sandbox_function(sum_from_rust, 3, 4);
 
-// // ANCHOR: echo-pre
-//   // Call the library echo function
-//   const char* helloStr = "hi hi!";
-//   size_t helloSize = strlen(helloStr) + 1;
-//   tainted_mylib<char*> taintedStr = sandbox.malloc_in_sandbox<char>(helloSize);
-//   strncpy(taintedStr
-//             .unverified_safe_pointer_because(helloSize, "writing to region")
-//          , helloStr, helloSize);
-// // ANCHOR_END: echo-pre
-// // ANCHOR: echo
-//   sandbox.invoke_sandbox_function(echo, taintedStr);
-//   sandbox.free_in_sandbox(taintedStr);
-// // ANCHOR_END: echo
+// ANCHOR: echo-pre
+  // Call the library echo function
+  const char* helloStr = "hi hi!";
+  size_t helloSize = strlen(helloStr) + 1;
+  tainted_mylib<char*> taintedStr = sandbox.malloc_in_sandbox<char>(helloSize);
+  strncpy(taintedStr
+            .unverified_safe_pointer_because(helloSize, "writing to region")
+         , helloStr, helloSize);
+// ANCHOR_END: echo-pre
+// ANCHOR: echo
+  sandbox.invoke_sandbox_function(echo, taintedStr);
+  sandbox.free_in_sandbox(taintedStr);
+// ANCHOR_END: echo
 
-// // ANCHOR: call_cb
-//   // register callback and call it
-//   auto cb = sandbox.register_callback(hello_cb);
-//   sandbox.invoke_sandbox_function(call_cb, cb);
-// // ANCHOR_END: call_cb
+// ANCHOR: call_cb
+  // register callback and call it
+  auto cb = sandbox.register_callback(hello_cb);
+  sandbox.invoke_sandbox_function(call_cb, cb);
+// ANCHOR_END: call_cb
 
 // ANCHOR: main-end
   // destroy sandbox
@@ -93,13 +89,13 @@ int main(int argc, char const *argv[]) {
 }
 // ANCHOR_END: main-end
 
-// // ANCHOR: callback
-// void hello_cb(rlbox_sandbox_mylib& _, tainted_mylib<const char*> str) {
-//   auto checked_string =
-//     str.copy_and_verify_string([](unique_ptr<char[]> val) {
-//         assert(val != nullptr && strlen(val.get()) < 1024);
-//         return move(val);
-//     });
-//   printf("hello_cb: %s\n", checked_string.get());
-// }
-// // ANCHOR_END: callback
+// ANCHOR: callback
+void hello_cb(rlbox_sandbox_mylib& _, tainted_mylib<const char*> str) {
+  auto checked_string =
+    str.copy_and_verify_string([](unique_ptr<char[]> val) {
+        assert(val != nullptr && strlen(val.get()) < 1024);
+        return move(val);
+    });
+  printf("hello_cb: %s\n", checked_string.get());
+}
+// ANCHOR_END: callback
