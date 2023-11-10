7d97ce34fec5d92d9708a719a93b526a80c5be02
Allen Aby <allen.aby@gmail.com>
2023-11-10 03:33:21 -0500

    able to pass correct flags to linker and run Rust from RLBox

--- main.cpp
+++ main.cpp
@@ -36,20 +36,6 @@ int main(int argc, char const *argv[]) {
   rlbox_sandbox_rust_from_c sandbox;
   sandbox.create_sandbox();
 
-//   // Call the library hello function:
-//   sandbox.invoke_sandbox_function(hello);
-// // ANCHOR_END: hello
-
-// // ANCHOR: add
-//   // call the add function and check the result:
-//   auto ok = sandbox.invoke_sandbox_function(add, 3, 4)
-//                    .copy_and_verify([](unsigned ret){
-//     printf("Adding... 3+4 = %d\n", ret);
-//     return ret == 7;
-//   });
-//   printf("OK? = %d\n", ok);
-// // ANCHOR_END: add
-
 // ANCHOR: sum_from_rust
   // call the sum_from_rust function and check the result:
   auto ok = sandbox.invoke_sandbox_function(sum_from_rust, 3, 4)
@@ -63,31 +49,6 @@ sandbox.invoke_sandbox_function(hello_from_rust);
 
 sandbox.invoke_sandbox_function(filewrite_from_rust);
 
-// hello_from_rust();
-// int sum = sum_from_rust(3, 4);
-// printf("sum is %d", sum);
-// sandbox.invoke_sandbox_function(sum_from_rust, 3, 4);
-
-// // ANCHOR: echo-pre
-//   // Call the library echo function
-//   const char* helloStr = "hi hi!";
-//   size_t helloSize = strlen(helloStr) + 1;
-//   tainted_mylib<char*> taintedStr = sandbox.malloc_in_sandbox<char>(helloSize);
-//   strncpy(taintedStr
-//             .unverified_safe_pointer_because(helloSize, "writing to region")
-//          , helloStr, helloSize);
-// // ANCHOR_END: echo-pre
-// // ANCHOR: echo
-//   sandbox.invoke_sandbox_function(echo, taintedStr);
-//   sandbox.free_in_sandbox(taintedStr);
-// // ANCHOR_END: echo
-
-// // ANCHOR: call_cb
-//   // register callback and call it
-//   auto cb = sandbox.register_callback(hello_cb);
-//   sandbox.invoke_sandbox_function(call_cb, cb);
-// // ANCHOR_END: call_cb
-
 // ANCHOR: main-end
   // destroy sandbox
   sandbox.destroy_sandbox();
@@ -96,13 +57,3 @@ sandbox.invoke_sandbox_function(filewrite_from_rust);
 }
 // ANCHOR_END: main-end
 
-// // ANCHOR: callback
-// void hello_cb(rlbox_sandbox_mylib& _, tainted_mylib<const char*> str) {
-//   auto checked_string =
-//     str.copy_and_verify_string([](unique_ptr<char[]> val) {
-//         assert(val != nullptr && strlen(val.get()) < 1024);
-//         return move(val);
-//     });
-//   printf("hello_cb: %s\n", checked_string.get());
-// }
-// // ANCHOR_END: callback
